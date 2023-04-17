27db5e171917a35b9542b4331c041b75dc84034a
CJPerez-Dev <77761058+CJPerez-Dev@users.noreply.github.com>
2023-04-17 17:11:07 -0600

    Finished dynamic stats pane

--- src/Stats.js
+++ src/Stats.js
@@ -1,48 +1,79 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 import styles from "./Stats.module.css";
 
-function Stats() {
-    return (
+const Stats = () => {
+  const [stats, setStats] = useState([]);
+
+
+  const getAllStats = async () => {
+    try {
+      const response = await axios.get('http://localhost:8080/stats');
+      const data = response.data;
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getAllStats();
+        setStats(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+    {stats.length > 0 ? (
       <div>
-        <h1 className={styles["stats-header"]}>Character name:</h1>
-        <div style={{ display: "flex" }}>
-          <div style={{ marginRight: "20px" }}>
-            <h2>Stats:</h2>
-            <table className={styles["stats-table"]}>
-              <tbody>
-                <tr>
-                  <td>hp:</td>
-                  <td>1</td>
-                </tr>
-                <tr>
-                  <td>attack:</td>
-                  <td>2</td>
-                </tr>
-                <tr>
-                  <td>defense:</td>
-                  <td>3</td>
-                </tr>
-              </tbody>
-            </table>
+        <Box
+        sx={{
+              display: "flex",
+              flexDirection: "column",
+              flexGrow: 1,
+              m: 1,
+              height: "100%",
+            }}>
+          {stats.map((stat) => (
+            <div key={stat.statsId}>
+              <Typography fontWeight={"bold"} fontSize={32} color={"white"}>
+                Profile Name: {stat.profile.profileName}
+              </Typography>
+              <Typography fontWeight={"bold"} fontSize={22} color={"white"}>
+                Class Type: {stat.profile.classType}
+              </Typography>
+              <Typography fontWeight={"bold"} fontSize={22} color={"white"}>
+                Gender: {stat.profile.gender}
+              </Typography>
+              <Typography fontWeight={"bold"} fontSize={22} color={"white"}>
+                HP: {stat.hp}
+              </Typography>
+              <Typography fontWeight={"bold"} fontSize={22} color={"white"}>
+                Attack: {stat.attack}
+              </Typography>
+              <Typography fontWeight={"bold"} fontSize={22} color={"white"}>
+                Defense: {stat.defense}
+              </Typography>
+              <Typography fontWeight={"bold"} fontSize={22} color={"white"}>
+                XP: {stat.xp}
+              </Typography>
           </div>
-          <div>
-            <h2>Current Level:</h2>
-            <table className={styles["stats-table"]}>
-              <tbody>
-                <tr>
-                  <td>Level:</td>
-                  <td>1</td>
-                </tr>
-                <tr>
-                  <td>XP:</td>
-                  <td>2</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
+        ))}
+        </Box>
       </div>
-    );
-  }
-  
-  export default Stats;
+    ) : (
+      <p>Loading...</p>
+    )}
+  </div>
+  );
+}
+ 
+export default Stats;
